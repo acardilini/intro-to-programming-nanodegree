fe493a4b1188209d14eb16d8bfd52bcc754e3d3c
acardilini <a.cardilini@gmail.com>
2016-08-14 11:53:50 +1000

    Add all level options and optional attempts

--- stage2/my_madlibs_generator.py
+++ stage2/my_madlibs_generator.py
@@ -1,3 +1,9 @@
+# Text asking for number of wrong guesses.
+wrong_guesses_preamble = """
+It's up to you to decide if you want to make it even harder or easier. Type in the number of wrong guesses you allow yourself before you fail the game.
+
+  """
+
 # A list of replacement words to be passed in to the play game function.
 easy_preamble = """
 You've chosen to take it easy this time and give your brain a chance to warm up. Good for you.
@@ -11,20 +17,53 @@ Good choice! This will definitely test you, but if you know your stuff you'll ge
 
 Fill in the blanks below:"""
 parts_of_medium = ["__1__", "__2__", "__3__", "__4__"]
-medium_answer = ["panic", "inputs", "outputs", "optimize"]
+medium_answer = ["boxes", "CSS", "selector", "attribute"]
 
+
+hard_preamble = """
+I like your style - Go Hard or Go Home!
+
+Fill in the blanks below:"""
 parts_of_hard = ["__1__", "__2__", "__3__", "__4__"]
-hard_answer = ["panic", "inputs", "outputs", "optimize"]
+hard_answer = ["dynamic", "flexible", "validate", "refine"]
 
 # The following are some test strings to pass in to the play_game function.
 warm_up = """
-An good guide for approaching problems is: 0. Don't __1__!!, 1. What are the __2__?, 2. What are the __3__?, 3. Work through some examples by hand., 4. Simple mechanical solutions, 5. Don't __4__ prematurely! Simple and correct is best, only do it when you need to.
+A good guide for approaching coding problems is:
+    0. Don't __1__!!,
+    1. What are the __2__?,
+    2. What are the __3__?,
+    3. Work through some examples by hand,
+    4. Simple mechanical solutions,
+    5. Don't __4__ prematurely! Simple and correct is best, only do it when you need to.
 
 """
 
-make_me_thinking = """"""
+make_me_think = """
+The internet is made up of a whole lot of __1__. The style of a HTML page is defined by the __2__ language. In order to 'speak' with the HTML page you need to identify each element using a __3__. The style of the element is then controlled by providing a value for the __4__ of the element you want to change.
 
-brain_sweats = """"""
+"""
+
+brain_sweats = """
+With the range of web browsing divices available, from desktops to smart phones, webpages must be __1__. A webpage should be built in a way that allows for __2__ positioning of its elements. Before celebrating a coding job well done it is important that you __3__ your code to check that everything works in different environments. Remembering the process of 'code, test & __4__' will help you build a functional webpage.
+
+"""
+
+# Congatulations message
+congrats = """
+
+Congratulations, you got it right!!
+
+"""
+
+# Try again message
+try_again = """
+
+Sorry, you've run out of attempts! Perhaps your brain needs a rest.
+
+Have a think and try again later.
+
+"""
 
 # Checks if a word in parts_of_speech is a substring of the word passed in.
 def word_in_pos(word, parts_of_speech):
@@ -36,6 +75,7 @@ def word_in_pos(word, parts_of_speech):
 # Plays a full game of mad_libs. A player is prompted to replace words in ml_string,
 # which appear in parts_of_speech with their own words.
 def play_game(ml_string, parts_of_speech, answer, preamble):
+    guesses = int(raw_input(wrong_guesses_preamble))
     print preamble
     print ml_string
     replaced = []
@@ -43,9 +83,13 @@ def play_game(ml_string, parts_of_speech, answer, preamble):
     answer_no = 0
     for word in ml_string:
         replacement = word_in_pos(word, parts_of_speech)
+        print word # TEST CODE - REMOVE!!
+
+        # Need to figure out how to print the paragraph with the correct word afte the user put in the correct word. This may require knowing the position of the word and replacing it in the origial ml_string. 
+
         if replacement != None:
-            attempt = 3
-            while attempt > 1:
+            attempt = guesses
+            while attempt >= 1:
                 user_input = raw_input("What should " + replacement + " be replaced with? ")
                 if user_input == answer[answer_no]:
                     word = word.replace(replacement, user_input)
@@ -53,8 +97,11 @@ def play_game(ml_string, parts_of_speech, answer, preamble):
                     answer_no = answer_no + 1
                     print """Congratulations, you got it right!!"""
                     break
+                if attempt == 1:
+                    print try_again
+                    return
                 else:
-                    print "Ooops"
+                    print "Ooops, try again."
                     attempt = attempt - 1
         else:
             replaced.append(word)
@@ -65,15 +112,18 @@ def play_game(ml_string, parts_of_speech, answer, preamble):
 # MY CODE
 # Choosing a game level.
 def game_choice():
-    user_input = raw_input("""Please choose how easy you want to take it:
-     Warming Up, Now I'm Thinking or Brain Sweats
+    difficulty = raw_input("""
+Please choose how easy you want to take it - enter the corresponding number 1, 2 or 3:
 
-     """)
-    if user_input == "Warm up":
+ 1. Warm Up, 2. Make Me Thinking or 3. Brain Sweats
+
+  """)
+
+    if difficulty == "1":
         play_game(warm_up, parts_of_easy, easy_answer, easy_preamble)
-    if user_input == "Make me think":
-        play_game(make_me_think, parts_of_speech1, medium_answer, meadium_preamble)
-    if user_input == "Brain Sweats":
-        play_game(brain_sweats, parts_of_speech1, hard_answer, hard_preamble)
+    if difficulty == "2":
+        play_game(make_me_think, parts_of_medium, medium_answer, meadium_preamble)
+    if difficulty == "3":
+        play_game(brain_sweats, parts_of_hard, hard_answer, hard_preamble)
 
 print game_choice()
